94ee8b8edae15fd7864b3e00da9e20b76d10c7da
nigamehsan <nigamehsan@10.216.79.212>
2021-09-07 12:27:54 -0400

    final version

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,11 @@
-
+import com.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.io.FileWriter;
 import java.net.URLConnection;
 
 /*
@@ -23,24 +24,20 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-   URLConnection conn;
+        
+        CSVWriter writer = new CSVWriter(new FileWriter(new File("data.csv")));
+        String[] header= new String[2];
+        header[0] = "Hello";
+        header[1] = "Hi";
+        
+          writer.writeNext(header);
+        
     
-          
-            conn = new URL( "https://cdn.islamic.network/quran/audio/128/ar.alafasy/798.mp3").openConnection();
-              InputStream is = conn.getInputStream();
-              
-    File file = new File( "798.mp3"); 
-
-     OutputStream outstream = new FileOutputStream(file);
-     byte[] buffer = new byte[4096];
-     int len;
-     while ((len = is.read(buffer)) > 0) {
-        outstream.write(buffer, 0, len);
-    }
-    outstream.close();
-      
-    }
+    
+    
     
                }
+    
+}
     
 
